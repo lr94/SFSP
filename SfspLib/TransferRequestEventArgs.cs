b4a673496ebe419205b9733dd33b04f0c332a6be
lr94 <lucarobbiano@gmail.com>
2017-06-30 21:43:45 +0200

    Aggiunta classe TransferRequestEventArgs

---
+++ SfspLib/TransferRequestEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Sfsp
+{
+    public class TransferRequestEventArgs : EventArgs
+    {
+        internal TransferRequestEventArgs(SfspAsyncDownload download)
+        {
+            Download = download;
+        }
+
+        public SfspAsyncDownload Download
+        {
+            get;
+            private set;
+        }
+    }
+}
