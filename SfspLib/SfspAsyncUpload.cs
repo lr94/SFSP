1459328088096e7853406a5729d7acb55abf3a25
lr94 <lucarobbiano@gmail.com>
2017-06-30 14:55:04 +0200

    Aggiungo un po' di documentazione

--- SfspLib/SfspAsyncUpload.cs
+++ SfspLib/SfspAsyncUpload.cs
@@ -5,6 +5,9 @@ using Sfsp.Messaging;
 
 namespace Sfsp
 {
+    /// <summary>
+    /// Rappresenta il trasferimento verso un altro host di un insieme di oggetti (file e cartelle)
+    /// </summary>
     public class SfspAsyncUpload : ISfspAsyncTransfer
     {
         private object locker = new object();
@@ -23,14 +26,23 @@ namespace Sfsp
             this.Status = TransferStatus.New;
         }
 
+        /// <summary>
+        /// Evento sollevato ad ogni cambio di stato
+        /// </summary>
         public event EventHandler<TransferStatusChangedEventArgs> StatusChanged;
 
+        /// <summary>
+        /// Avvia l'upload di tutti gli oggetti selezionati per il trasferimento
+        /// </summary>
         public void Start()
         {
             
         }
 
         private long _progress;
+        /// <summary>
+        /// Numero di byte già trasferiti (vengono contati solo i byte di contenuto dei file da trasferire)
+        /// </summary>
         public long Progress
         {
             get
@@ -52,6 +64,9 @@ namespace Sfsp
         }
 
         TransferStatus _status;
+        /// <summary>
+        /// Stato attuale del trasferimento
+        /// </summary>
         public TransferStatus Status
         {
             get
@@ -72,12 +87,18 @@ namespace Sfsp
             }
         }
 
+        /// <summary>
+        /// Numero totale dei byte da trasferire (inteso come "dimensione di tutti i file messi insieme", le cartelle non contano)
+        /// </summary>
         public long TotalSize
         {
             get;
             private set;
         }
 
+        /// <summary>
+        /// Interrompe il trasferimento in corso chiudendo la connessione con l'host remoto
+        /// </summary>
         public void Abort()
         {
             throw new NotImplementedException();
