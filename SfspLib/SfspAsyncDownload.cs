ecb47de60f02891df01dbf9d9b161dd10a693e1e
lr94 <lucarobbiano@gmail.com>
2017-06-30 21:23:44 +0200

    Creata nuova classe SfspAsyncDownload, implementa l'interfaccia ISfspAsyncTransfer

---
+++ SfspLib/SfspAsyncDownload.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Sfsp
+{
+    public class SfspAsyncDownload : ISfspAsyncTransfer
+    {
+        public long Progress
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public TransferStatus Status
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public long TotalSize
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public event EventHandler<TransferStatusChangedEventArgs> StatusChanged;
+
+        public void Abort()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
