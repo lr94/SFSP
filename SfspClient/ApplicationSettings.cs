394dcfd996da0731adb94b29ea8d752f859c91a9
lr94 <lucarobbiano@gmail.com>
2017-07-09 00:29:41 +0200

    Implementata gestione delle impostazioni

--- SfspClient/ApplicationSettings.cs
+++ SfspClient/ApplicationSettings.cs
@@ -19,23 +19,53 @@ namespace SfspClient
 
         private Dictionary<string, string> data;
 
-        public string GetApplicationFolder()
+        public ApplicationSettings()
+        {
+            HostName = System.Net.Dns.GetHostName();
+            DefaultPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Downloads");
+            AutoAccept = false;
+            Mode = HostMode.Online;
+
+            Load();
+        }
+
+        private string GetApplicationFolder()
         {
             string appdata = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
             return Path.Combine(appdata, ProductName);
         }
 
-        public string GetFileName()
+        private string GetFileName()
         {
             return Path.Combine(GetApplicationFolder(), SettingsFile);
         }
 
+        private void CreateFile()
+        {
+            string appdata = GetApplicationFolder();
+            if (!Directory.Exists(appdata))
+                Directory.CreateDirectory(appdata);
+
+            if (!File.Exists(GetFileName()))
+                Store();
+        }
+
+        /// <summary>
+        /// Carica nuovamente le impostazioni
+        /// </summary>
         public void Load()
         {
+            string fname = GetFileName();
+
+            // Se il file delle impostazioni non esiste lo creo
+            if (!File.Exists(fname))
+                CreateFile();
+
+            // Carico le impostazioni nella mappa
             data = new Dictionary<string, string>();
 
-            StreamReader sr = File.OpenText(GetFileName());
-
+            StreamReader sr = File.OpenText(fname); // UTF-8
+            
             while(!sr.EndOfStream)
             {
                 string line = sr.ReadLine().Trim();
@@ -50,9 +80,129 @@ namespace SfspClient
                 string value = line.Substring(equal_index + 1).Trim();
 
                 data.Add(key, value);
-
-                System.Windows.MessageBox.Show(key + "\n" + value);
             }
+
+            sr.Close();
+
+            // Imposto le proprietà
+
+            bool set_default = false; // Indica se dobbiamo impostare delle proprietà di default
+
+            // Se name o path sono vuote salviamo i dati di default determinati dinamicamente
+            string val = ReadString("name");
+            if (String.IsNullOrEmpty(val))
+                set_default = true;
+            else
+                HostName = val;
+
+            val = ReadString("path");
+            if (String.IsNullOrEmpty(val))
+                set_default = true;
+            else
+                DefaultPath = val;
+
+            AutoAccept = ReadBoolean("autoaccept");
+
+            Mode = ReadHostMode("mode");
+
+            if (set_default)
+                Store();
+        }
+
+        /// <summary>
+        /// Salva le impostazioni correntemente rappresentate dall'oggetto
+        /// </summary>
+        public void Store()
+        {
+            WriteString("name", HostName);
+            WriteString("path", DefaultPath);
+            WriteHostMode("mode", Mode);
+            WriteBoolean("autoaccept", AutoAccept);
+
+            StreamWriter sw = new StreamWriter(GetFileName(), false, Encoding.UTF8);
+
+            sw.WriteLine("# Do not manually edit this file");
+            foreach (string k in data.Keys)
+                sw.WriteLine(k + " = " + data[k]);
+
+            sw.Close();
+        }
+
+        private void WriteString(string key, string value)
+        {
+            data[key] = "\"" + value + "\"";
+        }
+
+        private void WriteInteger(string key, int value)
+        {
+            data[key] = value.ToString();
+        }
+
+        private void WriteBoolean(string key, bool value)
+        {
+            if (value)
+                data[key] = "true";
+            else
+                data[key] = "false";
+        }
+
+        private void WriteHostMode(string key, HostMode value)
+        {
+            if (value == HostMode.Online)
+                data[key] = "online";
+            else if (value == HostMode.Offline)
+                data[key] = "offline";
+        }
+
+        private string ReadString(string key)
+        {
+            if (!data.ContainsKey(key))
+                return "";
+
+            string val = data[key];
+
+            if (!(val.StartsWith("\"") && val.EndsWith("\"")))
+                throw new FileFormatException("Invalid string in settings file");
+
+            return val.Substring(1, val.Length - 2);
+        }
+
+        private bool ReadBoolean(string key)
+        {
+            if (!data.ContainsKey(key))
+                return false;
+
+            string val = data[key].ToLower();
+
+            if (val == "true")
+                return true;
+            else if (val == "false")
+                return false;
+            else
+                throw new FileFormatException("Invalid boolean in settings file");
+        }
+
+        private HostMode ReadHostMode(string key)
+        {
+            if (!data.ContainsKey(key))
+                return HostMode.Online;
+
+            string val = data[key].ToLower();
+
+            if (val == "online")
+                return HostMode.Online;
+            else if (val == "offline")
+                return HostMode.Offline;
+            else
+                throw new FileFormatException("Invalid host mode in settings file");
+        }
+
+        private int ReadInteger(string key)
+        {
+            if (!data.ContainsKey(key))
+                return 0;
+
+            return Int32.Parse(data[key]);
         }
 
         public string HostName
