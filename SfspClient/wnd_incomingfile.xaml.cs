ebcffd9bc302b12cccf177990f92b06bd9dd3297
lr94 <lucarobbiano@gmail.com>
2017-07-10 01:01:43 +0200

    Perfezionata la finestra di dialogo per l'accettazione dei file, adesso può essere usata

--- SfspClient/wnd_incomingfile.xaml.cs
+++ SfspClient/wnd_incomingfile.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Sfsp;
 
 namespace SfspClient
 {
@@ -19,9 +20,45 @@ namespace SfspClient
     /// </summary>
     public partial class wnd_incomingfile : Window
     {
-        public wnd_incomingfile()
+        public wnd_incomingfile(SfspAsyncDownload download, string defaultPath)
         {
             InitializeComponent();
+
+            txt_path.Text = defaultPath;
+            txtb_name.Text = String.Format(txtb_name.Text, download.RemoteHostName);
+            txtb_filename.Text = String.Format(txtb_filename.Text, download.GetObjects()[0]);
+            txtb_size.Text = String.Format(txtb_size.Text, NumericFormatter.FormatBytes(download.TotalSize));
+        }
+
+        public string DestinationPath
+        {
+            get
+            {
+                return txt_path.Text;
+            }
+        }
+
+        private void btn_accept_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void btn_cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+
+        private void btn_browse_Click(object sender, RoutedEventArgs e)
+        {
+            var fbd = new System.Windows.Forms.FolderBrowserDialog();
+
+            if(System.IO.Directory.Exists(txt_path.Text))
+                fbd.SelectedPath = txt_path.Text;
+
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                txt_path.Text = fbd.SelectedPath;
         }
     }
 }
