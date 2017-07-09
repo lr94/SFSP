134df53f5df21d516c56ecc70071edaff3ed80c9
lr94 <lucarobbiano@gmail.com>
2017-07-09 10:17:37 +0200

    Nuova finestra per accettare o rifiutare i file in arrivo

---
+++ SfspClient/wnd_incomingfile.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SfspClient
+{
+    /// <summary>
+    /// Logica di interazione per wnd_incomingfile.xaml
+    /// </summary>
+    public partial class wnd_incomingfile : Window
+    {
+        public wnd_incomingfile()
+        {
+            InitializeComponent();
+        }
+    }
+}
