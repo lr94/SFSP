3b664f647928575d5fda42ff7b03b956ef276fe0
lr94 <lucarobbiano@gmail.com>
2017-07-11 14:12:55 +0200

    Nuova classe SfspNetworkStream da usare al posto di NetworkStream

---
+++ SfspLib/TcpUtils/SfspNetworkStream.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace Sfsp.TcpUtils
+{
+    internal class SfspNetworkStream : NetworkStream
+    {
+        public SfspNetworkStream(Socket socket) : base(socket)
+        {
+        }
+
+        public SfspNetworkStream(Socket socket, bool ownSocket) : base(socket, ownSocket)
+        {
+        }
+
+        public SfspNetworkStream(Socket socket, System.IO.FileAccess fileAccess) : base(socket, fileAccess)
+        {
+        }
+
+        public SfspNetworkStream(Socket socket, System.IO.FileAccess fileAccess, bool ownSocket) : base(socket, fileAccess, ownSocket)
+        {
+        }
+
+        /// <summary>
+        /// Ottiene il socket sottostante
+        /// </summary>
+        /// <returns></returns>
+        public Socket GetSocket()
+        {
+            return this.Socket;
+        }
+    }
+}
